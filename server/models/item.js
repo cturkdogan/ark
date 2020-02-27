5e2edebe3c9d889d25000ff5c7eac735273af06a
Cagdas Turkdogan <turkdogan.cagdas@gmail.com>
2020-02-27 07:31:48 -0800

    Delete item.js

--- server/models/item.js
+++
@@ -1,9 +0,0 @@
-module.exports=(sequelize, Sequelize) => {
-    const Item = sequelize.define('Item', {
-        itemID: {type: Sequelize.STRING, unique: true, allowNull: false},
-        name: {type: Sequelize.STRING},
-        group: {type: Sequelize.STRING},
-        inventory: {type: Sequelize.INTEGER},
-    });
-    return Item;
-};
