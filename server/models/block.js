690bcb49d6a2b282957437ecf20d42b6484b27d7
Cagdas Turkdogan <turkdogan.cagdas@gmail.com>
2020-02-27 07:34:26 -0800

    Delete block.js

--- server/models/block.js
+++
@@ -1,7 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  const Block = sequelize.define('Block', {
-    blockchain: {type:DataTypes.STRING, allowNull:false},
-    register_date: {type:DataTypes.DATE, allowNull:false},
-  });
-  return Block;
-};
